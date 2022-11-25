fbc54269a356db1287d3efe5b58d082cd02b128a
Moses Githinji <ndirangugithinji23@gmail.com>
2022-11-25 22:52:42 +0300

    first-commit

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      @btn-click="$emit('toggle-add-task')"
+      text="Add Task"
+      color="steelblue"
+    />
+  </header>
+</template>
+
+<script>
+import Button from './Button.vue';
+export default {
+  name: 'Header',
+  props: {
+    title: String,
+  },
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-for="task in tasks" v-bind:key="task.id">
+    <Task
+      @toggle-reminder="$emit('toggle-reminder', task.id)"
+      @delete-task="$emit('delete-task', task.id)"
+      :task="task"
+    />
+  </div>
+</template>
+
+<script>
+import Task from './Task.vue';
+
+export default {
+  name: 'Tasks',
+  props: {
+    tasks: Array,
+  },
+  components: {
+    Task,
+  },
+  emits: ['delete-task', 'toggle-reminder'],
+};
+</script>
